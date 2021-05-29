ea2180c8f68f8909249a80ceea662299b3f32e36
TheBoringDude <iamcoderx@gmail.com>
2021-05-29 14:01:55 +0800

    move functions - move `new` command sub-commands to `new.go` - update minidb

--- cmd/new.go
+++ cmd/new.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+
+	"github.com/TheBoringDude/simple-store/cmd/internal"
 	"github.com/spf13/cobra"
 )
 
@@ -11,6 +14,51 @@ var newCmd = &cobra.Command{
 	Long:  `Create a new group.`,
 }
 
+// collectionsCmd represents the collections command
+var collectionsNewCmd = &cobra.Command{
+	Use:   "collections",
+	Short: "Create a new collections group",
+	Args:  internal.ValidateArg,
+	Long: `Create a new collections group.
+
+If there are multiple args passed, it
+will only get the first one.
+	
+EXAMPLE: store new collections my-collection`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cols := args[0]
+
+		db := internal.DB()
+		db.Collections(cols)
+
+		fmt.Printf("\nSuccessfully created a new collection: \033[1m%s\033[0m\n", cols)
+	},
+}
+
+// storeCmd represents the store command
+var storeNewCmd = &cobra.Command{
+	Use:   "store",
+	Short: "Create a new store group",
+	Args:  internal.ValidateArg,
+	Long: `Create a new store group.
+
+If there are multiple args passed, it
+will only get the first one.
+	
+EXAMPLE: store new store my-store`,
+	Run: func(cmd *cobra.Command, args []string) {
+		store := args[0]
+
+		db := internal.DB()
+		db.Store(store)
+
+		fmt.Printf("\nSuccessfully created a new store: \033[1m%s\033[0m\n", store)
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(newCmd)
+
+	/* sub-commands */
+	newCmd.AddCommand(collectionsNewCmd, storeNewCmd)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/TheBoringDude/simple-store
 go 1.16
 
 require (
-	github.com/TheBoringDude/minidb v0.1.2
+	github.com/TheBoringDude/minidb v0.1.3
 	github.com/spf13/cobra v1.1.3
 )
